2f1d3384b058b5eb11c11aca061f20119ad0cfb5
AnmolSharma0001 <anmol.as2002@gmail.com>
2023-07-01 21:56:11 +0530

    "Initial Commit"

--- community_walk/community_walk/asgi.py
+++ community_walk/community_walk/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for community_walks project.
+ASGI config for community_walk project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "community_walks.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "community_walk.settings")
 
 application = get_asgi_application()
---
+++ community_walk/footsafe/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class FootsafeConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "footsafe"
