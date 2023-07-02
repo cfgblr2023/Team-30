11c2400531d8018e9c98fc439f5a04c573af80c8
AnmolSharma0001 <92541992+AnmolSharma0001@users.noreply.github.com>
2023-07-02 07:50:32 +0530

    Delete community_walk directory

--- community_walk/footsafe/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class FootsafeConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "footsafe"
--- community_walk/footsafe/urls.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-from footsafe import views
-
-urlpatterns = [     
-    path('', views.home_view,name='home'),
-]
